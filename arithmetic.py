db85ec3bc34b72dd54e8608d05c5085f39ff7b93
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-16 21:56:09 -0700

    Final Code

---
+++ arithmetic.py
@@ -0,0 +1,58 @@
+"""Functions for common math operations."""
+
+
+def add(num1, num2):
+    """Return the sum of the two inputs."""
+    addition = num1 + num2
+
+    return addition
+
+
+def subtract(num1, num2):
+    """Return the second number subtracted from the first."""
+    sub = num1 - num2
+
+    return sub
+
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+    mult = num1*num2
+
+    return numlt
+
+
+def divide(num1, num2):
+    """Divide the first input by the second and return the result."""
+    div = num1/num2
+
+    return div
+
+def square(num1):
+    """Return the square of the input."""
+    sq = num1**num1
+
+    return sq
+
+
+def cube(num1):
+    """Return the cube of the input."""
+    c = num1**3
+
+    return c
+
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+    p = num1**num2
+
+    return p
+
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
+    modulo = num1%num2
+
+    return modulo
+
+
